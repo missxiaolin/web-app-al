0dd015b964b98d94f82c71a02dcc93030bb33dcd
xiaolin <462441355@qq.com>
2020-07-24 14:12:05 +0800

    * 增加预加载逻辑

--- extend/index.js
+++ extend/index.js
@@ -2,6 +2,12 @@ import mpExtend from './mp-extend.js'
 
 import simplePv from './simple/extend_pv.js'
 
+import extendPage from './complex/extend-page.js'
+
+// 以mpExtend(...)的形式引入多个扩展
+// 如果不需要某一个混入项的功能，直接注释掉即可
+mpExtend(extendPage)
+
 mpExtend(simplePv)
 
 export default {
