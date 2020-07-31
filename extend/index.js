9fee1eaac6bf5eba0c74116e257992ed7dd64d0e
xiaolin <462441355@qq.com>
2020-07-31 15:27:59 +0800

    * add

--- extend/index.js
+++ extend/index.js
@@ -5,12 +5,14 @@ import simplePv from './simple/extend_pv.js'
 import extendPage from './complex/extend-page.js'
 import extendGlobalData from './complex/extend-globalData.js'
 import extendMitt from './complex/extend-mitt.js'
+import extendOData from './complex/extend-oData.js'
 
 // 以mpExtend(...)的形式引入多个扩展
 // 如果不需要某一个混入项的功能，直接注释掉即可
 mpExtend(extendPage)
 mpExtend(extendGlobalData)
 mpExtend(extendMitt)
+mpExtend(extendOData)
 
 mpExtend(simplePv)
 
